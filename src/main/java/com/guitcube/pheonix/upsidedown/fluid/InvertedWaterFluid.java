735ced3e1635ed1beb85f6f520350ab4a4a6e893
Arcratist <58129974+Arcratist@users.noreply.github.com>
2019-11-24 20:36:21 +1100

    Update InvertedWaterFluid.java

--- src/main/java/com/guitcube/pheonix/upsidedown/fluid/InvertedWaterFluid.java
+++ src/main/java/com/guitcube/pheonix/upsidedown/fluid/InvertedWaterFluid.java
@@ -11,41 +11,13 @@ import net.minecraftforge.fluids.ForgeFlowingFluid;
 
 public abstract class InvertedWaterFluid extends ForgeFlowingFluid {
 
-	private boolean enableSourceCondense;
-	private boolean enableSourceFloat = true;
-	private int maxHeight = 120;
-	private int densityDir = 1;
-
 	public InvertedWaterFluid(Properties properties) {
 		super(properties);
 	}
 
 	@Override
 	public void tick(World worldIn, BlockPos pos, IFluidState state) {
-		int i = this.func_215667_a(worldIn, pos, state, state);
-		if (state.isSource()) {
-		//	if (worldIn.rand.nextInt(3) == 0) {
-//
-				if (shouldSourceBlockFloat(worldIn, pos)) {
-					worldIn.setBlockState(pos.add(0, densityDir, 0), this.getDefaultState().getBlockState(), 3);
-					worldIn.getPendingFluidTicks().scheduleTick(pos.add(0, densityDir, 0), state.getFluid(), i);
-					worldIn.notifyNeighborsOfStateChange(pos.add(0, densityDir, 0), state.getBlockState().getBlock());
-					return;
-		//		}
-			}
-		}
 
-		//worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
-
-		//this.flowAround(worldIn, pos, state);
-
-		System.out.println("IWF");
-	}
-
-	private boolean shouldSourceBlockFloat(World world, BlockPos pos) {
-
-		BlockState state = world.getBlockState(pos.add(0, densityDir, 0));
-		return enableSourceFloat && (state != this.getBlockState(getDefaultState()));
 	}
 
 	public static class Flowing extends InvertedWaterFluid {
